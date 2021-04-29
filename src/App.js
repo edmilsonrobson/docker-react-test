186cfc969da0d2dabdcd7d7ffd0920a78eb7882f
Ed <edmilsonrobson@gmail.com>
2021-04-29 14:50:15 -0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Do you even lift, react?
         </a>
       </header>
     </div>
