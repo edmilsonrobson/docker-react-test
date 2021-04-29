eaa138fb1fa87591d014d171439dba9ce2dda212
Ed <edmilsonrobson@gmail.com>
2021-04-29 15:10:00 -0300

    Fixes test

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Do you even lift, react?
+          Learn React
         </a>
       </header>
     </div>
